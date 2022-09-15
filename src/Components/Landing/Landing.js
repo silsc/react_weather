15abf92543e7fd37207c372ef31261346cad55c9
Silvia Salat <salat.silvia@gmail.com>
2022-09-15 19:01:40 +0200

    FIX(langing) responsiveness ADD(Fade in animation)

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,26 +1,35 @@
 import './landing.scss';
 import world from "../../assets/images/world.png"
 import Searchbox from '../Searchbox/Searchbox';
+import FadeIn from 'react-fade-in';
 
 function Landing(props) {
   return (
-    <div className="landing">
-      <div className="hero">
-        <div className="hero-title">
-          <h1>Weather</h1>
-          <h1>today</h1>
+    <FadeIn>
+      <div className="landing">
+        <div className="hero">
+          <div className="hero-title">
+            <h1>Weather</h1>
+            <h1>today</h1>
+          </div>
+          <FadeIn delay={500}>
+            <div className='hero-img-wrapper'>
+              <img src={world} alt="World" className="hero-img"/>
+            </div>
+          </FadeIn>
+          <div className="search">
+            <h2>Check the current weather</h2>
+            <Searchbox search={props.search} query={props.query} setquery={props.setquery}/>
+          </div>
         </div>
-        <img src={world} alt="World" className="hero-img"/>
-        <div className="hero-date">
-          <h3>Today is</h3> 
-          <h3>{props.date}</h3> 
-        </div>
+        <FadeIn delay={800}>
+          <div className="landing-date">
+              <h3 className="landing-date-title">Today is</h3> 
+              <h3>{props.date}</h3> 
+          </div>
+        </FadeIn>
       </div>
-      <div className="search">
-        <h2>Check the current weather</h2>
-        <Searchbox search={props.search} query={props.query} setquery={props.setquery}/>
-      </div>
-    </div>
+    </FadeIn>
   );
 }
 
