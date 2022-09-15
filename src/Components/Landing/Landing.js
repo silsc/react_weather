3a2149ae57d874d0d400ac979a13c1f98caf300b
Restyled.io <commits@restyled.io>
2022-09-15 17:02:22 +0000

    Restyled by clang-format

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,36 +1,32 @@
 import './landing.scss';
+
+import FadeIn from 'react-fade-in';
+
 import world from "../../assets/images/world.png"
 import Searchbox from '../Searchbox/Searchbox';
-import FadeIn from 'react-fade-in';
 
 function Landing(props) {
   return (
-    <FadeIn>
-      <div className="landing">
-        <div className="hero">
-          <div className="hero-title">
-            <h1>Weather</h1>
+      <FadeIn><div className = "landing"><div className = "hero">
+      <div className = "hero-title"><h1>Weather</h1>
             <h1>today</h1>
-          </div>
+      </div>
           <FadeIn delay={500}>
             <div className='hero-img-wrapper'>
               <img src={world} alt="World" className="hero-img"/>
-            </div>
-          </FadeIn>
-          <div className="search">
-            <h2>Check the current weather</h2>
-            <Searchbox search={props.search} query={props.query} setquery={props.setquery}/>
-          </div>
-        </div>
-        <FadeIn delay={800}>
-          <div className="landing-date">
-              <h3 className="landing-date-title">Today is</h3> 
-              <h3>{props.date}</h3> 
-          </div>
-        </FadeIn>
       </div>
-    </FadeIn>
-  );
+          </FadeIn><div className = "search">
+      <h2>Check the current weather<
+          /h2>
+            <Searchbox search={props.search} query={props.query} setquery={props.setquery}/>
+      </div>
+        </div><FadeIn delay = {800}>
+      <div className = "landing-date"><h3 className = "landing-date-title">
+          Today is</h3> 
+              <h3>{props.date}</h3>
+      </div>
+        </FadeIn></div>
+    </FadeIn>);
 }
 
 export default Landing;
