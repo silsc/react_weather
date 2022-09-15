39ff6f0ae72007130672b9c3a26cb9b7276f168e
Restyled.io <commits@restyled.io>
2022-09-15 17:02:27 +0000

    Restyled by prettier

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,6 +1,6 @@
-import './results.scss';
+import "./results.scss";
 
-import FadeIn from 'react-fade-in';
+import FadeIn from "react-fade-in";
 
 import clear from "../../assets/images/clear.png";
 import cloudy from "../../assets/images/cloudy.png";
@@ -8,7 +8,7 @@ import rain from "../../assets/images/rain.png";
 import snow from "../../assets/images/snow.png";
 import world from "../../assets/images/world.png";
 
-function Results({weather, date}) {
+function Results({ weather, date }) {
   let todayweather;
   let bgImg;
   if (weather.weather[0].main === "Clouds") {
@@ -32,20 +32,38 @@ function Results({weather, date}) {
   }
 
   return (
-      <FadeIn><div className = "results"><div className = "results-info">
-      <h2 className = "location">{weather.name},
-      {weather.sys.country}</h2>
+    <FadeIn>
+      <div className="results">
+        <div className="results-info">
+          <h2 className="location">
+            {weather.name},{weather.sys.country}
+          </h2>
           <h3 className="date">{date}</h3>
-      </div>
+        </div>
         <FadeIn delay={500}>
-          <div className="weather" style={{width: "300px", height: "200px" ,backgroundImage: `url(${bgImg})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}}>
+          <div
+            className="weather"
+            style={{
+              width: "300px",
+              height: "200px",
+              backgroundImage: `url(${bgImg})`,
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
             <h2 className="weather-temp">{Math.round(weather.main.temp)}ºC</h2>
-      </div>
-        </FadeIn><FadeIn delay = {800}><div className = "today"><p>
-      <span>Today: </span> {todayweather}</p></div>
+          </div>
+        </FadeIn>
+        <FadeIn delay={800}>
+          <div className="today">
+            <p>
+              <span>Today: </span> {todayweather}
+            </p>
+          </div>
         </FadeIn>
       </div>
-    </FadeIn>);
+    </FadeIn>
+  );
 }
 
 export default Results;
